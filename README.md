189f394237d1bce011dab3020bf56bae6a0f1855
ethanbayliss <ethan@ethanbayliss.com>
2020-08-01 21:28:03 +0800

    starting rest connection

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ https://api.up.com.au/ &amp; https://api.youneedabudget.com/
 
 # usage
 cp config/template.json config/config.json
+
 pip install -r requirements.txt
-python run.py --config config.json
+
+python run.py
--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-#%%
+#imports
 import conf
+import requests
+import json
 
+#conf
+conf.load("config.json")
+
+#constants
+UP_API_ENDPOINT = "https://api.up.com.au/api/v1"
+
+#creds
+UP_CREDS_HEADER = {"Authorization": "Bearer {}".format(conf.get("upbankApiKey"))}
+
+#curl https://api.up.com.au/api/v1/util/ping \
+#  -H "Authorization: Bearer $your_access_token"
+
+def main():
+    """Main function ran on script startup"""
+    testUpbankApi()
+
+def testUpbankApi():
+    """Make a call to up bank's API and parse result to determine if key is correct"""
+    resp = requests.get("{}/util/ping".format(UP_API_ENDPOINT),headers=UP_CREDS_HEADER)
+    if resp.status_code == 200:
+        print("Up Bank Connected: {}".format(json.loads(resp.content)["meta"]["statusEmoji"]))
+    if resp.status_code != 200:
+        print("Up Bank connection failed: GET /util/ping {}".format(resp.status_code))
+
+def testYnabApi():
+    """Make a call to YNAB API to test key"""
+
+if __name__ == "__main__":
+    main()
